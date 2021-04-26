3a49a714dc9f3ce5d74ce16066ea1fd8f26d98c2
Taimoor Khan <taimoor36372@gmail.com>
2021-04-26 00:09:00 -0700

    update

--- packages/frontity-chakra-theme/src/components/archive/post-preview.js
+++ packages/frontity-chakra-theme/src/components/archive/post-preview.js
@@ -5,28 +5,33 @@ import Link from "../link";
 import { styled ,css} from "frontity";
 import {decode} from "frontity";
 import {formatDate} from "../helpers";
+import { useMediaQuery } from "@chakra-ui/react"
 const PostPreview = ({ data, ...rest }) => {
   const { title, excerpt, featured_media, link} = data;
+  
+  const [isSmallerThan900] = useMediaQuery("(max-width: 900px)")
   return (
     <Flex
-      direction="row"
+      direction={isSmallerThan900 ?"column" :"row"} 
       position="relative"
       bg="white"
       box-shadow= "none"
       as="article"
-      shadow="md"
       {...rest}
     >
    
       {featured_media && featured_media.src && (
-          <Flex py="40px" pl="40px" pr="20px" flexGrow="1" direction="column" position="relative" width="50%">
+
+      <Flex py="40px" pl="40px" pr="20px" flexGrow="1" direction="column" position="relative"
+      width={isSmallerThan900 ?"100%" :"50%"}>
         <Link link={link}>
           <PostImageWithOverlay {...featured_media} />
         </Link>
-          </Flex>
+          </Flex> 
+        
       )}
 
-      <Flex p="40px"  pl="0px" flexGrow="1" direction="column" width="50%">
+      <Flex p="40px"  pl="0px" flexGrow="1" direction="column" width={isSmallerThan900 ?"100%" :"50%"} >
         <Flex direction="row" mb="5px">
           <Text fontSize="sm">
             <Link color="#767676" link={data.author.link}>
@@ -50,7 +55,7 @@ const PostPreview = ({ data, ...rest }) => {
           dangerouslySetInnerHTML={{ __html: excerpt }}
         />
   
-          <Link link={link} bg="#08f"  width={{ md: "150px" }} mt="5px" py="8px"  color="white" css={css` text-align: center ; border-radius: 4px;  &:hover {
+          <Link link={link} bg="#08f"  width={{ md: "100px" }} mt="5px" py="8px"  fontSize="sm"  color="white" css={css` text-align: center ; border-radius: 4px;  &:hover {
             background-color: #036fcd;
          
           &:focus {
@@ -62,7 +67,8 @@ const PostPreview = ({ data, ...rest }) => {
           justify="flex-start"
           categories={categories}
         />*/}
-
+        <Text>
+      </Text>
       </Flex>
     </Flex>
   );
@@ -80,3 +86,13 @@ const StyledDiv = styled.div`
   }
 }
 `;
+
+const StyledDiv2 = styled.div`
+display: grid;
+grid-template-columns: 1fr ;
+// same as:
+// grid-template-columns: repeat(3, 1fr);
+grid-gap: 8px
+
+`;
+
