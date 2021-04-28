db15444065e5339075c3ced687ea36c43a0efa0e
Taimoor Khan <taimoor36372@gmail.com>
2021-04-28 05:07:12 -0700

    sub menu

--- packages/frontity-chakra-theme/src/components/header/navigation.js
+++ packages/frontity-chakra-theme/src/components/header/navigation.js
@@ -2,8 +2,33 @@ import { Box, Stack } from "@chakra-ui/react";
 import { styled } from "frontity";
 import React from "react";
 import FrontityLink from "../link";
+import { css } from "frontity";
 
 const Link = styled(FrontityLink)`
+  position: relative;
+  color: #fff;
+  text-decoration: none;
+
+  &:after {
+    transition: bottom ease 0.25s, background-color ease 0.25s;
+    content: "";
+    width: 100%;
+    height: 2px;
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    background: transparent;
+  }
+
+  &:hover {
+    &:after {
+      bottom: -5px;
+      background-color: ${p => p.theme.colors.accent[400]};
+    }
+  }
+`;
+
+const Link2 = styled(FrontityLink)`
   position: relative;
   color: #fff;
   text-decoration: none;
@@ -58,38 +83,83 @@ const SiteMenuItem = ({ link, ...props }) => (
   </Box>
 );
 
+const SiteMenuItem2 = ({ link, ...props }) => (
+  <Box
+    as="li"
+    color="white"
+    fontSize={"10px"}
+    fontWeight="medium"
+    fontFamily="Kelson"
+    textTransform="uppercase"
+    position="relative"
+    cursor="pointer"
+    {...props}
+  >
+    <Link2 link={link}>{props.children}</Link2>
+  </Box>
+);
+
 const Navigation = ({ menu, ...props }) => (
   <Box as="nav" width="100%" display={{ base: "none", lg: "block" }} {...props}>
     <SiteMenu>
       {menu.map(({name, link,submenu}) => (
-        <div>
-        <SiteMenuItem key={name} link={link}>
+        <div class="mmenu" css={css`
+        position:relative;`
+      }>
+        <SiteMenuItem  key={name} link={link}>
           {name} 
         </SiteMenuItem>
-        { submenu &&  submenu.map(({name, link,}) => {
-          return (
 
-            <SiteMenuItem key={name} link={link}>
-          {name} 
-        </SiteMenuItem>
+
+        <MenuItem class="innermenu" key={name} css={css`
+        position: absolute;
+        top: 47px;
+        background: #000;
+        
+        `}>
+        { submenu &&  submenu.map(({name, link,}) => {
+          
+          return (
+            <div>
+            
+            <SiteMenuItem2 key={name} link={link}>
+        <div css={css` 
+         position : relative
+       
+       
+      } `}>{name} </div>  
+        </SiteMenuItem2>
            
          
-            
-          );
+   
+      
+        </div> );
         })
-      } </div>
+      } 
+      </MenuItem>
+
+      </div>
       ))}
+    
     </SiteMenu>
   </Box>
 );
 
 export default Navigation;
 
+const MenuItem = styled.ul`
+font-size: 10px ! important
+border : 2px solid
+background : black
+position : relative
+list-style : none;
+
+
+`;
 const StyledMenu = styled.ul`
   display: flex;
   flex-direction: ${({submenu}) => submenu && 'column'};
   visibility: ${({submenu}) => submenu && 'hidden'};
-  position: ${({submenu}) => submenu && 'absolute'};
   font-size: 1.8rem;
   font-weight: 500;
   letter-spacing: -0.0277em;
@@ -99,8 +169,10 @@ const StyledMenu = styled.ul`
   margin: ${({submenu}) => submenu ? '10px' : 0};
   width: ${({submenu}) => submenu && '100px'};
 
-
-
+  ${MenuItem}:hover & {
+    visibility: ${({submenu}) => submenu && 'visible'};
+  }
+ 
   @media (min-width: 1220px) {
     margin-top: ${({submenu}) => submenu ? '10px' : '-0.8rem'}; ;
     margin-right: 0px;
