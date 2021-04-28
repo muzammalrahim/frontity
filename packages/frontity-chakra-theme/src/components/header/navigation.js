469b3344742c435fe55606feddd94ea13765c4a8
Taimoor Khan <taimoor36372@gmail.com>
2021-04-27 23:21:40 -0700

    sub menu

--- packages/frontity-chakra-theme/src/components/header/navigation.js
+++ packages/frontity-chakra-theme/src/components/header/navigation.js
@@ -62,12 +62,49 @@ const Navigation = ({ menu, ...props }) => (
   <Box as="nav" width="100%" display={{ base: "none", lg: "block" }} {...props}>
     <SiteMenu>
       {menu.map(({name, link,submenu}) => (
+        <div>
         <SiteMenuItem key={name} link={link}>
           {name} 
         </SiteMenuItem>
+        { submenu &&  submenu.map(({name, link,}) => {
+          return (
+
+            <SiteMenuItem key={name} link={link}>
+          {name} 
+        </SiteMenuItem>
+           
+         
+            
+          );
+        })
+      } </div>
       ))}
     </SiteMenu>
   </Box>
 );
 
 export default Navigation;
+
+const StyledMenu = styled.ul`
+  display: flex;
+  flex-direction: ${({submenu}) => submenu && 'column'};
+  visibility: ${({submenu}) => submenu && 'hidden'};
+  position: ${({submenu}) => submenu && 'absolute'};
+  font-size: 1.8rem;
+  font-weight: 500;
+  letter-spacing: -0.0277em;
+  flex-wrap: wrap;
+  justify-content: flex-end;
+  list-style: none;
+  margin: ${({submenu}) => submenu ? '10px' : 0};
+  width: ${({submenu}) => submenu && '100px'};
+
+
+
+  @media (min-width: 1220px) {
+    margin-top: ${({submenu}) => submenu ? '10px' : '-0.8rem'}; ;
+    margin-right: 0px;
+    margin-bottom: 0px;
+    margin-left: -2.5rem;
+  }
+`;
